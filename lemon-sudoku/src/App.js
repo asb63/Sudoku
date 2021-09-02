4ffc52272399a02f96b961b3953f5f1a329c9b11
Andrew Brooks <asb63@txstate.edu>
2021-09-02 18:32:31 -0500

    hide logo on click

--- lemon-sudoku/src/App.js
+++ lemon-sudoku/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game/Game';
+import { useState } from 'react'
 
 function App() {
-  
+
+  //allows logo to be hidden on click
+  const [logoHidden, setLogoHidden] = useState(false);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        {logoHidden ? (
+            console.log('logo is hidden')
+          ) : (
+            <img src={logo} className="App-logo" alt="logo" onClick={() => setLogoHidden(true)}/>
+          )}
       </header>
       
       <Game className="Game"></Game>
